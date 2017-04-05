76c4c684998b884ff310e54a08b2b0bf4a4724c5
rlarsonil71 <ron.larson@libertymutual.com>
2017-04-05 15:33:19 -0400

    Added modal as nav bar
    Within nav bar, the modal will ask the user for multiple items.
    This nav bar/modal construct was copied from the BOOTSTRAP website.
    
    Added functionality to `name` entry within modal to log input to
    console log.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,10 +5,21 @@ const config = require('./config')
 
 $(() => {
   setAPIOrigin(location, config)
+  $('form').on('submit', function (event) {
+    console.log('Inside submit handler for Name field of MODAL')
+
+    event.preventDefault()
+
+    const input = $('#name').val()
+    console.log('Name entered is: ', input)
+
+    // Hide the modal from displaying to the suer
+    $('#myModal').modal('hide')
+  })
 })
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
-require('./example')
+// require('./example')
